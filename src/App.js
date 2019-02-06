1556fef0b2f0119a093bbed285f7a3f3227b5c1f
KaMa75 <karolmarszalek@o2.pl>
2019-02-06 20:33:45 +0100

    Rozwiązanie problemów z zależnościami oraz prace nad headerem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './resources/styles.css';
+
+import Header from './components/header_footer/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        test
+      <div className="App" style={{ height: "120vh", background: 'pink'}}>
+        <Header/>
       </div>
     );
   }
